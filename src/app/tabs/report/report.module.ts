035dc2b1c1ab22a4154df104b0a849cba1a2d905
jaya gupta <jgpt0510@gmail.com>
2022-06-28 11:00:20 +0530

    report design modification

--- src/app/tabs/report/report.module.ts
+++ src/app/tabs/report/report.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,8 @@ import { ReportPage } from './report.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
+    FormsModule,
     ReportPageRoutingModule
   ],
   declarations: [ReportPage]
--- src/app/tabs/report/report.page.ts
+++ src/app/tabs/report/report.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from 'src/app/services/account/account.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-report',
@@ -11,9 +13,25 @@ export class ReportPage implements OnInit {
 
   constructor(
     public accountServices: AccountService,
+    public _http: HttpClient,
     public router: Router
   ) { }
-
+  addReport = new FormGroup({
+    fromdate: new FormControl('',Validators.required),
+    todate: new FormControl('',Validators.required)
+  });
+  get fromdate(){
+    return this.addReport.get('fromdate');
+  }
+  get todate(){
+    return this.addReport.get('todate');
+  }
+  genReport(){
+    this._http.get<any>("http://localhost:3000/CRM_APP", this.addReport.value)
+    .subscribe(res=>{
+       alert("okay");
+    })
+  }
   ngOnInit() {
   }
   public onClickLogout() {
