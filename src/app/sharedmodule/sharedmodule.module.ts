e6a13769855408e5696fee02dc7df9b5fdf7b476
unknown <surajkumar.4892@gmail.com>
2022-08-29 18:54:13 +0530

    Changes in resources

--- src/app/sharedmodule/sharedmodule.module.ts
+++ src/app/sharedmodule/sharedmodule.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from 'src/app/header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [HeaderComponent]
 })
